2560dbe3976f38ec68ea3aeabee00453b5ceb7a1
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2015-06-18 21:14:56 -0500

    Prior to Bayesian analysis of Arbelaez' p8425.

--- src/+mlnest/Lighthouse.m
+++ src/+mlnest/Lighthouse.m
@@ -32,6 +32,13 @@ classdef Lighthouse < mlnest.AbstractApply
     %  Information: H = 3.11403 nats = 4.49259 bits
     %  mean(x) = 1.27463, stddev(x) = 0.173153
     %  mean(y) = 0.990962, stddev(y) = 0.178608
+    
+    %  For D = D + 1:    
+    % # iterates = 1000
+    % Evidence: ln(Z) = -162.927 +- 0.195109
+    % Information:  H = 3.80677 nats = 5.492 bits
+    % mean(x) = 1.91361, stddev(x) = 0.0753362
+    % mean(y) = 1.09591, stddev(y) = 0.21325
 
 	%  $Revision$ 
  	%  was created $Date$ 
